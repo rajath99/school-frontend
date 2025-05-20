5c1bbb1000f6f593fa837ee267210fb4a30b01c4
rajath99 <84463733+rajath99@users.noreply.github.com>
2025-05-21 00:34:29 +0530

    Update environment.js

--- src/environment.js
+++ src/environment.js
@@ -4,7 +4,7 @@
 const netlifyDeployedApiUrl = import.meta.env.VITE_API_BASE_URL; // This VAR NAME MUST MATCH Netlify setting
 const localApiUrl = "http://localhost:5001/api"; // Your local backend with /api
 
-export const baseUrl = netlifyDeployedApiUrl || localApiUrl;
+//export const baseUrl = netlifyDeployedApiUrl || localApiUrl;
 
 // CRITICAL DEBUG LOGS - Keep these for now
 console.log("ENVIRONMENT.JS: Raw VITE_API_BASE_URL from Netlify env:", import.meta.env.VITE_API_BASE_URL);
